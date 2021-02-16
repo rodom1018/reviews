9cf3cbb5561112b063f6a0b78155879d7324f2a1
rodom <enjoying1018@naver.com>
2021-02-16 14:27:57 +0900

    users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("user_name",)
+    list_filter = (
+        "preference",
+        "favour_book",
+        "favour_movie",
+        "language",
+    )
+
+    fieldsets = (
+        ("Info", {"fields": ("user_name", "language", "preference", "bio")}),
+        ("Favour", {"fields": ("favour_book", "favour_movie")}),
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django_countries.fields import CountryField
 
 # Create your models here.
+class User(models.Model):
+
+    MOVIE = "movie"
+    BOOK = "books"
+
+    PREFERENCE_CHOICES = (
+        (MOVIE, "Movie"),
+        (BOOK, "Book"),
+    )
+
+    MALE = "male"
+    FEMALE = "female"
+
+    GENDER_CHOICES = (
+        (MALE, "male"),
+        (FEMALE, "female"),
+    )
+
+    user_name = models.CharField(max_length=30, blank=True)
+    favour_book = models.CharField(max_length=30)
+    favour_movie = models.CharField(max_length=30)
+    language = CountryField()
+    preference = models.CharField(max_length=20, choices=PREFERENCE_CHOICES)
+    bio = models.CharField(max_length=20, choices=GENDER_CHOICES)
